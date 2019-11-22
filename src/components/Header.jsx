a9795512e61a7c84d4f98bafe22f77029ca113b4
Maksym Sivko <maksym.sivko@gmail.com>
2019-11-22 19:16:19 +0200

    +

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://png.pngtree.com/element_pic/00/16/07/115783931601b5c.jpg"
+                className="logo"
+                alt="logo"
+            />
+        </header>
+    );
+};
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://www.pond5.com/images/images_db/vlp/image-hero-poster.jpg"
+                    alt=""
+                />
+            </div>
+
+            <div>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia,
+                consequatur?
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
